fd0c35a3a95ff0c42aa76fc91b4004ad6665259f
Arnold Atchoe <kwameape123@gmail.com>
2024-09-28 19:52:26 -0500

    added more sql files

--- database_creator.py
+++ database_creator.py
@@ -1,7 +1,11 @@
 # Import needed module or libraries
-import pandas as pd
+import pandas as pd # External module
 import sqlite3
 import pathlib
+import logging
+
+# Configure logging to write to a file, appending new logs to the existing file
+logging.basicConfig(filename='log.txt', level=logging.DEBUG, filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Create a file path for the database
 db_file = pathlib.Path('books.db')
@@ -15,6 +19,7 @@ def create_database():
     except sqlite3.Error as e:
         print(f"Database creation Error: {e}")
 
+
 def create_table():
     try:
         with sqlite3.connect(db_file) as connection:
@@ -26,9 +31,23 @@ def create_table():
     except sqlite3.Error as e:
         print(f'Table creation Error: {e}')
 
+def insert_record():
+    try:
+        with sqlite3.connect(db_file) as connection:
+            sql_file = pathlib.Path('sql', 'insert_records.sql')
+            with open(sql_file, 'r') as file:
+                sql_scripts = file.read()  # Add parentheses to call the read method
+                connection.executescript(sql_scripts)
+                print('records has been inserted')
+    except sqlite3.Error as e:
+        print(f'Records insertion Error: {e}')
+
 def main():
+    logging.info("Program started")
     create_database()
     create_table()
+    insert_record()
+    logging.info("Program ended")
 
 if __name__ == "__main__":
     main()
--- folder_script.py
+++ folder_script.py
@@ -5,6 +5,10 @@ for datafun-05-sql'''
 import pathlib as pl
 import os
 import csv
+import logging
+
+# Configure logging to write to a file, appending new logs to the existing file
+logging.basicConfig(filename='log_folder_script.txt', level=logging.DEBUG, filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Declare global variables
 root_directory = pl.Path.cwd()
@@ -18,11 +22,25 @@ def csv_file(filename:str)->None:
     with open(file_path,mode = 'w', newline = '') as file:
         pass
 
+def sql_file(filename:str)->None:
+    file_path = root_directory/sql_directory/filename
+    with open(file_path,mode = 'w', newline = '') as file:
+        pass
+
 
 
 def main()->None:
-    csv_file('books.csv')
-    csv_file('authors.csv')
+    logging.info("Program Started")
+    # csv_file('books.csv')
+    # csv_file('authors.csv')
+    sql_file('update_records.sql')
+    sql_file('delete_records.sql')
+    sql_file('query_aggregation.sql')
+    sql_file('query_filter.sql')
+    sql_file('query_sorting.sql')
+    sql_file('query_group.sql')
+    sql_file('query_join.sql')
+    logging.info("Program, ended")
 
 if __name__ == '__main__':
     main()
